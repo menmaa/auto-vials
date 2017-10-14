803df1dec39ba63de72f83b952459a77c8de0a93
menmaa <31330913+menmaa@users.noreply.github.com>
2017-10-14 10:15:32 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# auto-vials
+
+A tera-proxy module. Automatically logs in to all characters and uses "Vial of Elinu's Tears" from each character's inventory.
+
+### Prerequisites
+
+* [command](https://github.com/pinkipi/command) - by Pinkie Pie
+
+### Usage
+
+`/proxy autovials` or `/8 autovials` to enable or disable the module.
+
+To stop it you should cancel the return to character selection and run the command afterwards.
+
+### Additional Information
+
+* The module will login to a character whether or not it has Vial of Elinu's Tears in the inventory. If it doesn't find any, or it finds but it is on cooldown, it will simply proceed to the next character.
+* The module will prevent you from selecting a character manually while it is running, for additional safety.
+* When the module has finished its job, it will return to the character it was started from. To interrupt this process simply click cancel on the "Returning to character selection" message.
+* You can pause the process by cancelling the return to character selection. The process will continue if you manually return to character selection later.
+* It is recommended to change the values of `ACTION_DELAY` and `CHAR_SELECT_DELAY` according to your needs. The reason these exist is to avoid any potential client bugs and make the module behave closer to a human by delaying the actions a bit instead of directly executing them.
---
+++ index.js
@@ -0,0 +1,172 @@
+const Command = require('command');
+const { protocol } = require('tera-data-parser');
+
+const VIAL_ID = 182433;							// Vial of Elinu's Tears ID
+const ACTION_DELAY = 1000;						// Delay between different actions in milliseconds
+const CHAR_SELECT_DELAY = 10000;				// Delay between retrieving the character list and selecting the character in milliseconds
+
+if(!protocol.messages.has('C_CANCEL_RETURN_TO_LOBBY')) {
+	protocol.messages.set('C_CANCEL_RETURN_TO_LOBBY', new Map().set(1, []));
+}
+
+module.exports = function autoVials(dispatch) {
+	
+	const command = new Command(dispatch);
+	
+	let enabled = false,
+		cooldown = false,
+		cid = null,
+		playerId = null,
+		itemId = null,
+		itemAmount = null,
+		chars = null,
+		charsUsed = [],
+		returnToChar = null,
+		cdTimer = null,
+		charSelectTimer = null
+		
+		
+	dispatch.hook('S_LOGIN', 4, (event) => {
+		({cid} = event);
+		({playerId} = event);
+	});
+	
+	dispatch.hook('S_INVEN', 9, (event) => {
+		let invenList = event.items;
+		
+		for(i = 0; i < invenList.length; i++) {
+			if(invenList[i].dbid == VIAL_ID) {
+				itemId = invenList[i].id.low;
+				itemAmount = invenList[i].amount;
+				break;
+			}
+		}
+	});
+	
+	dispatch.hook('S_START_COOLTIME_ITEM', 1, (event) => {
+		if(event.item == VIAL_ID) {
+			cooldown = true;
+			cdTimer = setTimeout(function() { cooldown = false }, event.cooldown * 1000);
+		}
+	});
+	
+	dispatch.hook('S_RETURN_TO_LOBBY', 1, (event) => {
+		cooldown = false;
+		itemId = null;
+		itemAmount = null;
+		clearTimeout(cdTimer);
+	});
+	
+	dispatch.hook('C_SELECT_USER', 1, (event) => {
+		if(enabled || returnToChar) {
+			return false;
+		}
+	});
+	
+	dispatch.hook('C_CANCEL_RETURN_TO_LOBBY', 1, (event) => {
+		if(!enabled && returnToChar) {
+			returnToChar = null;
+			command.message('Interrupted return to the starting character.');
+		}
+	});
+	
+	dispatch.hook('S_GET_USER_LIST', 5, (event) => {
+		if(!charSelectTimer) {
+			chars = event.characters;
+		
+			if(enabled) {
+				for(i = 0; i < chars.length; i++) {
+					if(charsUsed.indexOf(chars[i].id) == -1) {
+						let charid = chars[i].id;
+						
+						charSelectTimer = setTimeout(function() {
+							dispatch.toServer('C_SELECT_USER', 1, {
+								id: charid,
+								unk: 0
+							});
+							
+							dispatch.hookOnce('C_PLAYER_LOCATION', 1, (event) => {
+								setTimeout(useVial, ACTION_DELAY);
+							});
+							
+							charSelectTimer = null;
+						}, CHAR_SELECT_DELAY);
+						
+						console.log(`[Auto Vials] Next character selected: ${chars[i].name} (ID: ${chars[i].id})`);
+						break;
+					}
+				}
+			} else if(returnToChar) {
+				charSelectTimer = setTimeout(function() {
+					dispatch.toServer('C_SELECT_USER', 1, {
+						id: returnToChar,
+						unk: 0
+					});
+					charSelectTimer = null;
+					returnToChar = null;
+				}, CHAR_SELECT_DELAY);
+				console.log(`[Auto Vials] Returning to the starting character...`);
+			}
+		}
+	});
+	
+	command.add('autovials', () => {
+		if(!enabled) {
+			enabled = true;
+			returnToChar = playerId;
+			command.message('Auto Vials enabled.');
+			console.log(`[Auto Vials] Auto Vials enabled.`);
+			useVial();
+		} else {
+			disableAutoVial();
+			returnToChar = null;
+		}
+	});
+	
+	function useVial() {
+		if(!cooldown && itemAmount > 0) {
+			dispatch.toServer('C_USE_ITEM', 1, {
+				ownerId: cid,
+				item: VIAL_ID,
+				id: itemId,
+				unk1: 0,
+				unk2: 0,
+				unk3: 0,
+				unk4: 1,
+				unk5: 0,
+				unk6: 0,
+				unk7: 0,
+				x: 0,
+				y: 0,
+				z: 0,
+				w: 0,
+				unk8: 0,
+				unk9: 0,
+				unk10: 0,
+				unk11: 0
+			});
+			command.message('Vial of Elinu\'s Tears used.');
+		}
+		charsUsed.push(playerId);
+		
+		if(chars.length > charsUsed.length) {
+			command.message('Switching characters...');
+			setTimeout(returnToLobby, ACTION_DELAY);
+		} else {
+			disableAutoVial();
+			setTimeout(returnToLobby, ACTION_DELAY);
+			command.message('Returning to the starting character...');
+		}
+	}
+	
+	function returnToLobby() {
+		dispatch.toServer('C_RETURN_TO_LOBBY', 1);
+	}
+	
+	function disableAutoVial() {
+		enabled = false;
+		charsUsed = [];
+		command.message('Auto Vials disabled.');
+		console.log(`[Auto Vials] Auto Vials disabled.`);
+	}
+}
